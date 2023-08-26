6867ede4cdf1f258f06c84505a9802efc7565135
SomeQu <bektemirovaidar@gmail.com>
2023-08-27 02:31:45 +0600

    visual changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,14 @@
+import Cell from "./components/Cell/Cell";
 import "./App.scss";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import moment from "moment";
-import { useEffect, useState } from "react";
-import Cell from "./components/Cell/Cell";
 
 function App() {
-  const [dates, setDates] = useState<string[]>([]);
-  // Сегодняшняя дата
-  const today: string = moment().format("Y-MM-DD");
-  const [api, setApi] = useState<any>(null);
-  useEffect(() => {
-    // Цикл для заполнения
-    for (let i = 0; i <= 357; i++) {
-      setDates((dates) => [
-        ...dates,
-        moment().subtract(i, "days").format("Y-MM-DD"),
-      ]);
-    }
-    // Гет запрос на АПИ
-    setApi(
-      axios
-        .get("https://dpg.gg/test/calendar.json")
-        .then((response) => setApi(response.data))
-    );
-  }, [today]);
   function groupDatesByMonth(dates: string[]): Record<string, string[]> {
     const dateMap: Record<string, string[]> = {};
 
-    for (let dateStr of dates) {
+    for (const dateStr of dates) {
       const [year, month] = dateStr.split("-");
       const monthKey = `${year}-${month}`;
 
@@ -40,8 +21,40 @@ function App() {
 
     return dateMap;
   }
+
+  // Массив для заполнения датами
+  const [dates, setDates] = useState<string[]>([]);
+  // Сегодняшняя дата
+  const today: string = moment().format("Y-MM-DD");
+  const [api, setApi] = useState<any>(null);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Цикл для заполнения
+    for (let i = 0; i <= 356; i++) {
+      setDates((dates) => [
+        ...dates,
+        moment().subtract(i, "days").format("Y-MM-DD"),
+      ]);
+    }
+    if (dates.length > 0) {
+      const newDates = [...dates];
+      newDates[newDates.length - 1] = moment(newDates[newDates.length - 1])
+        .subtract(1, "day")
+        .format("Y-MM-DD"); // Сдвигаем последнюю дату на 1 день назад
+      setDates(newDates);
+    }
+    // Гет запрос на АПИ
+    setApi(
+      axios
+        .get("https://dpg.gg/test/calendar.json")
+        .then((response) => setApi(response.data))
+    );
+  }, [today]);
+
   const groupedDates = groupDatesByMonth(dates);
   const sortedMonthKeys = Object.keys(groupedDates).sort();
+
   return (
     <div className="App">
       <div className="day">
@@ -67,7 +80,6 @@ function App() {
         </div>
         <div className="example">
           <p>Меньше</p>
-          <Cell />
           <Cell match={9} />
           <Cell match={19} />
           <Cell match={29} />
